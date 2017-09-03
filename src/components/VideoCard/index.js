92761f1c4bc82d40531e86590d26ca5807dc4ff3
Alejo Moreno <tecksonic@gmail.com>
2017-09-03 00:07:49 -0500

    Video Portal: Add rating to home videos

--- src/components/VideoCard/index.js
+++ src/components/VideoCard/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {Col} from 'react-materialize'
 import {Link} from 'react-router-dom'
+import Rating from 'react-rating'
 import './VideoCard.css'
 
 class VideoCard extends Component {
@@ -18,6 +19,7 @@ class VideoCard extends Component {
     render() {                
         const {video} = this.props        
         let description = video.description.length > 70 ? video.description.substring(0,70)+' [...Read More]' : video.description
+        let rating = (video.ratings.reduce((a,b)=> a+b)/ video.ratings.length).toString().substr(0,3)
         return (             
                 <Col s={this.props.sCols} m={this.props.mCols} l={this.props.lCols}>
                     <div className="video-card z-depth-4">
@@ -27,6 +29,7 @@ class VideoCard extends Component {
                         <video className="responsive-video" preload="metadata" controls>
                             <source src={`../${video.url}`} type="video/mp4"/>
                         </video>
+                        <Rating initialRate={rating} full="fa fa-star" empty="fa fa-star-o" readonly />
                         <div className="video-desc">
                             <p>{description}</p>
                         </div>
